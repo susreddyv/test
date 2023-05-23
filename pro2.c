ff79095ff3c2e077aa3a8c4ca95e3eacec05eb32
sushmareddy <v.sushmareddy252@gmail.com>
2023-05-23 15:23:20 +0530

    first commit

---
+++ pro2.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("this is sushma");
+
+    return 0;
+}
+
---
+++ pro3.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("this is aswin");
+
+    return 0;
+}
+i
